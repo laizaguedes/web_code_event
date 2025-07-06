8358e194c89c73e774ae1e66811d41d537cfe7f8
laiza <laiza@wdtecnologia.com.br>
2025-07-06 14:59:00 -0300

    tentativa de correção

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // desabilita otimização para sites estáticos
+  },
 };
 
 export default nextConfig;
